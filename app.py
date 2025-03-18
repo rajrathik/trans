3b2ed86eb95a8903bf2ba4e9c59c6b859eb23478
rajrathik <98695264+rajrathik@users.noreply.github.com>
2025-03-18 17:09:15 -0400

    Update21 app.py

--- app.py
+++ app.py
@@ -100,8 +100,4 @@ def get_transactions():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+ 
