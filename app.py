bbd9f40dc23e61684c5390a0b8a2577b89a012c5
rajrathik <98695264+rajrathik@users.noreply.github.com>
2025-03-18 09:58:41 -0400

    Update app6.py

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def get_transactions():
         return jsonify({"error": "An internal error occurred"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
